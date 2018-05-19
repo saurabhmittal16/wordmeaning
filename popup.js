7750aa9b32a4ad19db82447c83d19046e0ad6caf
saurabhmittal16 <saurabhmittal1606@gmail.com>
2018-05-19 23:12:42 +0530

    Add popup HTML and JS

---
+++ popup.js
@@ -0,0 +1,10 @@
+let button = document.querySelector('#submit');
+
+button.addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            {code: 'let x=window.getSelection().toString(); console.log(x);'}
+        );
+    });
+});
