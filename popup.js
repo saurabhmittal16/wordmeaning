a0a18331d09042989f5fefa1af7f67ef60780208
saurabhmittal16 <saurabhmittal1606@gmail.com>
2018-05-20 00:03:12 +0530

    Read word selected

--- popup.js
+++ popup.js
@@ -1,10 +1,24 @@
-let button = document.querySelector('#submit');
+let button = document.querySelector('#submit'),
+    word = document.querySelector('#wordToSearch'),
+    meaning = document.querySelector('#meaning');
+
+
+
 
 button.addEventListener('click', () => {
+    meaning.style.display = 'block';
     chrome.tabs.query({active: true, currentWindow: true}, tabs => {
         chrome.tabs.executeScript(
             tabs[0].id,
-            {code: 'let x=window.getSelection().toString(); console.log(x); '}
+            {code: 'window.getSelection().toString();'},
+            selection => {
+                console.log(selection);
+
+                if (selection[0].length > 2){
+                    word.value = selection[0];
+                    // meaning.innerHTML = findMeaning(word.value);
+                } 
+            }
         );
     });
 });
