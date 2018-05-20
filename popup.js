b4f89302758b682427aec689eeee905ca2de9ca4
saurabhmittal16 <saurabhmittal1606@gmail.com>
2018-05-20 12:53:22 +0530

    Add meaning API and show meaning in popup

--- popup.js
+++ popup.js
@@ -2,8 +2,30 @@ let button = document.querySelector('#submit'),
     word = document.querySelector('#wordToSearch'),
     meaning = document.querySelector('#meaning');
 
+function meaningPromise(word) {
+    let findMeaning = new Promise(
+        function(resolve, reject) {
+            let request = new XMLHttpRequest();
+            let final = "";
+    
+            request.open('GET', `http://api.pearson.com/v2/dictionaries/entries?headword=${word}`, true);
+            request.onload = () => {
+                let content = JSON.parse(request.response);
+                if (request.status >= 200 && request.status < 400){
+                    resolve(content.results[0].senses[0].definition);
+                    console.log(content.results[0].senses[0].definition);
+                } else {
+                    reject("No such Word found");
+                }
+            };
+            request.send();
+        }
+    );
 
+    return findMeaning;
+}
 
+let wordToFind = "";
 
 button.addEventListener('click', () => {
     meaning.style.display = 'block';
@@ -14,10 +36,19 @@ button.addEventListener('click', () => {
             selection => {
                 console.log(selection);
 
-                if (selection[0].length > 2){
+                if (word.value.length > 0) {
+                    wordToFind = word.value;
+                } else if (selection[0].length > 2){
                     word.value = selection[0];
-                    // meaning.innerHTML = findMeaning(word.value);
-                } 
+                    wordToFind = word.value;
+                }
+                meaningPromise(wordToFind)
+                        .then(wordMeaning => {
+                            meaning.innerHTML = wordMeaning;
+                        })
+                        .catch(error => {
+                            meaning.innerHTML = error;
+                        });
             }
         );
     });
